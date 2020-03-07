48aa6970cba7bc8b148e3b01e58294bcc8be80af
kgkanatsios <kgkanatsios@gmail.com>
2020-03-07 19:51:10 +0200

    Install vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core';
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome';
+import {
+  fas
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import "@/assets/css/tailwind.css";
 
