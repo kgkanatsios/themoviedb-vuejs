7799049b450d17ca663ccb548ffc15ac03e6b1c7
kgkanatsios <kgkanatsios@gmail.com>
2020-03-14 23:03:25 +0200

    Add details on movie component

--- src/components/movies/Movie.vue
+++ src/components/movies/Movie.vue
@@ -2,14 +2,29 @@
   <div class="movie">
     <div v-if="!movieLoading">
       <div class="text-right my-6">
+        <a
+          target="_blank"
+          :href="movie.imdb_id | imdbLink"
+          class="hover:text-teal-400 text-teal-500 font-bold py-2 px-4 hover:border-teal-500 inline-block align-middle"
+        >
+          <font-awesome-icon icon="link" class="mr-1"></font-awesome-icon>
+          Show on IMDB
+        </a>
         <button
           @click="$router.go(-1)"
-          class="bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded"
+          class="bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded inline-block align-middle"
         >
           Back
         </button>
       </div>
-      {{ movie.title }}
+      <div class="bg-teal-900 text-white px-4 py-2 font-bold mb-4">
+        <span class="text-xl inline-block align-middle">{{ movie.title }}</span>
+        <span class="text-sm mx-2 inline-block align-middle"
+          >(<font-awesome-icon icon="calendar-alt"></font-awesome-icon>
+          {{ movie.release_date }})</span
+        >
+      </div>
+
       {{ movie.overview }}
     </div>
     <div v-else class="text-center">
--- src/filter/index.js
+++ src/filter/index.js
@@ -1,3 +1,6 @@
 export const charactersLimiter = (value, limit) => {
   return value.length > limit ? value.substring(0, limit) + "..." : value;
 };
+export const imdbLink = value => {
+  return "https://www.imdb.com/title/" + value + "/";
+};
