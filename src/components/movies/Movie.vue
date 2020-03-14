06778f7b679099703bec571194eb71f2159a140e
kgkanatsios <kgkanatsios@gmail.com>
2020-03-14 22:00:43 +0200

    Add loader to Movie template

--- src/components/movies/Movie.vue
+++ src/components/movies/Movie.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="movie">
-    <div class="text-right my-6">
-      <button
-        @click="$router.go(-1)"
-        class="bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded"
-      >
-        Back
-      </button>
+    <div v-if="!movieLoading">
+      <div class="text-right my-6">
+        <button
+          @click="$router.go(-1)"
+          class="bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded"
+        >
+          Back
+        </button>
+      </div>
+      {{ movie.title }}
+      {{ movie.overview }}
     </div>
-    {{ movie.title }}
-    {{ movie.overview }}
+    <div v-else class="text-center">
+      <div
+        class="bg-teal-500 hover:bg-teal-400 text-white py-2 px-4 hover:border-teal-500 rounded text-2xl inline-block"
+      >
+        <font-awesome-icon icon="spinner" class="fa-spin"></font-awesome-icon>
+      </div>
+    </div>
   </div>
 </template>
 
