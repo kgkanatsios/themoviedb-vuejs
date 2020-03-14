ca47856c9eeaa0a4051c59887359f13217ab04e3
kgkanatsios <kgkanatsios@gmail.com>
2020-03-14 23:05:25 +0200

    Add imdbLink filter to main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { charactersLimiter } from "./filter";
+import { charactersLimiter, imdbLink } from "./filter";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -19,6 +19,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 Vue.filter("charactersLimiter", charactersLimiter);
+Vue.filter("imdbLink", imdbLink);
 
 new Vue({
   router,
