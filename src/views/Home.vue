f976365e26e8b9acb459f727127bc179781e67f0
kgkanatsios <kgkanatsios@gmail.com>
2020-04-15 00:55:56 +0300

    add latest movie on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue" class="inline-flex" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <movie-preview v-if="!moviesLoading" :movie="latestMovie"></movie-preview>
+    <div v-else class="text-center my-6">
+      <div
+        class="bg-teal-500 hover:bg-teal-400 text-white py-2 px-4 hover:border-teal-500 rounded text-2xl inline-block"
+      >
+        <font-awesome-icon icon="spinner" class="fa-spin"></font-awesome-icon>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mapActions, mapGetters } from "vuex";
+import MoviePreview from "@/components/movies/MoviePreview.vue";
 
 export default {
   name: "Home",
+  computed: {
+    ...mapGetters(["latestMovie", "moviesLoading"])
+  },
+  methods: {
+    ...mapActions({
+      fetchLatestMovie: "fetchLatestMovie"
+    })
+  },
+  created() {
+    this.fetchLatestMovie();
+  },
   components: {
-    HelloWorld
+    MoviePreview
   }
 };
 </script>
