829a2a1b2fa122bee0208fe4ebe2bb6977c62316
kgkanatsios <kgkanatsios@gmail.com>
2020-04-15 02:53:05 +0300

    modify categories - add movies posters

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="categories">
-    <div class="flex bg-gray-200">
-      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+    <div class="flex -mx-2">
+      <div
+        v-for="(list, index) in lists"
+        :key="index"
+        class="flex-1 flex flex-wrap text-gray-700 text-center bg-gray-400 m-2"
+      >
         <router-link
-          v-if="movies.length > 0"
-          :to="{ name: 'Movie', params: { id: movies[0].id } }"
-          >Latest Movies</router-link
+          :to="/categories/ + list.link"
+          class="w-full text-lg p-2 transition duration-200 ease-in-out hover:bg-gray-800 hover:text-gray-200"
         >
-      </div>
-      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-        <router-link to="/categories/now_playing">Now Playing</router-link>
-      </div>
-    </div>
-    <div class="flex bg-gray-200">
-      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-        <router-link to="/categories/popular">Popular Movies</router-link>
-      </div>
-      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-        <router-link to="/categories/top_rated">Top Rated</router-link>
-      </div>
-      <div class="flex-1 text-gray-900 text-center bg-gray px-4 py-2 m-2">
-        <router-link to="/categories/upcoming">Upcoming</router-link>
+          {{ list.label }}</router-link
+        >
+        <router-link :to="/categories/ + list.link" class="flex flex-wrap">
+          <span
+            v-for="(sample, index) in list.samples.slice(0, 9)"
+            :key="index"
+            class="flex w-1/3"
+          >
+            <img
+              :src="imageChecker(sample.poster_path)"
+              :alt="sample.original_title"
+              class="opacity-50 hover:opacity-100 transition duration-200 ease-in-out"
+            />
+          </span>
+        </router-link>
+        <router-link
+          :to="/categories/ + list.link"
+          class="w-full p-2 transition duration-200 ease-in-out hover:bg-gray-800 hover:text-gray-200"
+        >
+          Show All</router-link
+        >
       </div>
     </div>
   </div>
@@ -28,19 +38,26 @@
 
 <script>
 import { mapActions, mapGetters } from "vuex";
+import { imageChecker } from "@/mixins";
 export default {
   name: "Categories",
   computed: {
-    ...mapGetters(["movies"])
+    ...mapGetters(["languageCurrent", "lists"])
   },
   methods: {
     ...mapActions({
-      fetchLatestMovie: "fetchLatestMovie"
+      fetchListsData: "fetchListsData"
     })
   },
+  watch: {
+    languageCurrent: function() {
+      this.fetchListsData();
+    }
+  },
   created() {
-    this.fetchLatestMovie();
-  }
+    this.fetchListsData();
+  },
+  mixins: [imageChecker]
 };
 </script>
 
